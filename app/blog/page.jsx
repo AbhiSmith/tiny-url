fd7795ddaa2e1eeb82ded6257e4714d1875c565c
AbhiSmith <abhishekv4004@gmail.com>
2024-02-27 23:35:47 +0530

    add

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -2,20 +2,20 @@ import getDomain from '@/lib/getDomain'
 
 async function getData() {
  
-  // const domain =getDomain();   
+  const domain =getDomain();   
 
-  // const endpoint = `${domain}/api/promt/` // -> third party api service request? 
+  const endpoint = `${domain}/api/promt/` // -> third party api service request? 
   
-  //   const res = await fetch(endpoint)
-  //   if (!res.ok) {
-  //       throw new Error('Failed to fetch data')    }
+    const res = await fetch(endpoint)
+    if (!res.ok) {
+        throw new Error('Failed to fetch data')    }
     
-  //   if (res.headers.get('content-type').includes('application/json')){
-  //     return {items: []}
-  //   }      
+    if (res.headers.get('content-type').includes('application/json')){
+      return {items: []}
+    }      
      
-  //   return res.json(); 
-    return {items: []}
+    return res.json(); 
+    // return {items: []}
 
 }
 
